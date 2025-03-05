2e4ed7b486e8a0d147ae57d4e869b0bfc1952155
hesham hamza <heshamhamza1997@gmail.com>
2025-03-05 22:42:36 +0200

    removing unused imports

--- src/main/java/org/example/perfomActions.java
+++ src/main/java/org/example/perfomActions.java
@@ -1,9 +1,7 @@
 package org.example;
-
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
 public class perfomActions {
