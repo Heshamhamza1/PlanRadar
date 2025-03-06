eba0141dd50e3e1ebecb96bf335bfd5aacdb0116
hesham hamza <heshamhamza1997@gmail.com>
2025-03-06 15:41:50 +0200

    fix

--- src/main/java/org/example/perfomActions.java
+++ src/main/java/org/example/perfomActions.java
@@ -8,15 +8,15 @@ public class perfomActions {
     public static void settingDefaultValues(WebDriver driver) throws InterruptedException {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
-        // Click on the ticket type element
+
         WebElement elements = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"ticket-type-list\"]/div[2]/div/div[2]/div/div[2]/div/div[1]/div[1]/div/span")));
         elements.click();
-        // Click on the title field (better XPath for stability)
+
         WebElement title = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[3]/div/div[1]/div/div[2]/div/div/div[2]/div[1]/div/div/div[1]/div/div[2]/span")));
         title.click();
-        // Wait for the input field using data-id
+
         WebElement defaultTitle = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@data-id='Setup_Forms_Title_DefaultValue']")));
-        // Ensure the field is editable before clearing
+
         Thread.sleep(2000);
         defaultTitle.clear();
         defaultTitle.sendKeys("Default Title");
@@ -25,7 +25,6 @@ public class perfomActions {
 
         driver.findElement(By.xpath("/html/body/div[3]/div/div[1]/div/div[2]/div/div/div[2]/div[1]/div/div/div[2]/div/div[2]/span")).click();
         WebElement shortText = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@data-id='Setup_Forms_ShortText_DefaultValue']")));
-        // Ensure the field is editable before clearing
         shortText.click();
         shortText.clear();
         Thread.sleep(1000);
@@ -34,7 +33,7 @@ public class perfomActions {
 
         driver.findElement(By.xpath("/html/body/div[3]/div/div[1]/div/div[2]/div/div/div[2]/div[1]/div/div/div[3]/div/div[2]/span")).click();
         WebElement longText = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@data-id='Setup_Forms_LongText_DefaultValue']")));
-        // Ensure the field is editable before clearing
+
         longText.click();
         longText.clear();
         Thread.sleep(1000);
@@ -42,7 +41,7 @@ public class perfomActions {
         Thread.sleep(1000);
         driver.findElement(By.xpath("/html/body/div[3]/div/div[1]/div/div[2]/div/div/div[2]/div[1]/div/div/div[4]/div/div[2]/span")).click();
         WebElement checkBox = wait.until(ExpectedConditions.elementToBeClickable(By.id("Checked by default")));
-        // Ensure the field is editable before clearing
+
         checkBox.click();
         Thread.sleep(1000);
         WebElement element = driver.findElement(By.xpath("/html/body/div[3]/div/div[1]/div/div[2]/div/div/div[2]/div[1]/div/div/div[5]/div/div[1]"));
